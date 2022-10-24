e8e5ed35329f71701e1594c31e3cea15a8d40367
James McClure <2385464M@student.gla.ac.uk>
2022-10-24 10:42:03 +0100

    Fiexed problem with gcc compiler, screen now outputs

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,8 @@ use amethyst::{
     },
     utils::application_root_dir,
 };
-use amethyst::winit::MouseCursor::Default;
+use core::default::Default;
+
 
 pub struct Pong;
 
@@ -15,6 +16,7 @@ impl SimpleState for Pong{}
 
 fn main() -> amethyst::Result<()>{
 
+    //Default::default()
     // Add logger so that we can see errors while code is running
     amethyst::start_logger(Default::default());
 
@@ -24,8 +26,18 @@ fn main() -> amethyst::Result<()>{
 
     // Setting up basic application setup
 
-    // INstance of central repo for all game logic
-    let game_data = GameDataBuilder::default();
+    // Instance of central repo for all game logic
+    let game_data = GameDataBuilder::default()
+        .with_bundle(
+            RenderingBundle::<DefaultBackend>::new()
+            // The RenderToWindow plugin provides all the scaffolding for opening a window and drawing on it
+                .with_plugin(
+                    RenderToWindow::from_config_path(display_config_path)?
+                        .with_clear([0.0, 0.0, 0.0, 1.0]),
+                )
+                // RenderFlat2D plugin is used to render entities with a `SpriteRender` component.
+                .with_plugin(RenderFlat2D::default()),
+        )?;
     let assets_dir = app_root.join("assets");
     let mut game = Application::new(assets_dir, Pong, game_data)?;
     game.run();
